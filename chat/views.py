817c9c34904208010ce2bcca93b0c2e485a62101
Cjjphat <phat.vo@kyanon.digital>
2021-09-28 10:32:48 +0700

    push code to repo

---
+++ chat/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html',{})
+def chatroom(request,room_name):
+    return render(request,'chatroom.html',{
+        'room_name':room_name
+    })
